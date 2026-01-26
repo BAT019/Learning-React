10374b62d43d469ffc5c3b662c7fde321345c977
Atharv Gupta <batcodes01@gmail.com>
2026-01-26 16:44:02 +0530

    Initiated usedebounce

--- lecture-7/src/App.jsx
+++ lecture-7/src/App.jsx
@@ -3,14 +3,7 @@ import { usePost } from "../hooks/usePost";
 import { useFetch } from "../hooks/useFetch";
 import { usePrev } from "../hooks/usePrev";
 function App(){
-  const[state, setState] = useState(0);
- const prev = usePrev(state);
- return(<div>
-   <p>{state}</p>
-   <button onClick={() => {setState(c => c+1)}}>Click</button>
-   <p>Previous value was: {prev}</p>
- </div>
- )
+  
 }
 export default App;
 
@@ -19,16 +12,28 @@ export default App;
 
 
 
-  // const [ currentPost, setcurrentPost] = useState(1);
-  // const {finaldata, loading} = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
-  // if(loading){
-  //   return <div>
-  //     Loading....
-  //   </div>
-  // }
-  // return <div>
-  // <button onClick={() => setcurrentPost(1)}>1</button>
-  // <button onClick={() => setcurrentPost(2)}>2</button>
-  // <button onClick={() => setcurrentPost(3)}>3</button>
-  // {JSON.stringify(finaldata)}
-  // </div>
+//-------------------UsePrev--------------
+//  const[state, setState] = useState(0);
+//  const prev = usePrev(state);
+//  return(<div>
+//    <p>{state}</p>
+//    <button onClick={() => {setState(c => c+1)}}>Click</button>
+//    <p>Previous value was: {prev}</p>
+//  </div>
+//  )
+
+
+//--------------------UseFetch---------------
+// const [ currentPost, setcurrentPost] = useState(1);
+// const {finaldata, loading} = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+// if(loading){
+//   return <div>
+//     Loading....
+//   </div>
+// }
+// return <div>
+// <button onClick={() => setcurrentPost(1)}>1</button>
+// <button onClick={() => setcurrentPost(2)}>2</button>
+// <button onClick={() => setcurrentPost(3)}>3</button>
+// {JSON.stringify(finaldata)}
+// </div>
